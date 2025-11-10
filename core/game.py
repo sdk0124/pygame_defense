2cc515ebb53211a0278f740a8e0e4475921bd17a
sdk <kimseong124@gmail.com>
2025-11-10 09:24:10 +0900

    first commit

---
+++ core/game.py
@@ -0,0 +1,29 @@
+import pygame
+from core.settings import FPS, SCREEN_WIDTH, SCREEN_HEIGHT
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        pygame.display.set_caption("CPU Defence")
+
+    def run(self):
+        while self.running:
+            self.clock.tick(FPS)
+            self.handle_events()
+            self.update()
+            self.draw()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill((255, 255, 255))
+        pygame.display.flip()
---
+++ core/settings.py
@@ -0,0 +1,9 @@
+ROWS = 15               # 맵 타일 가로 칸 개수
+COLS = 15               # 맵 타일 세로 칸 개수
+CELL_SIZE = 64          # 한 타일의 가로, 세로 크기 (정사각형)
+SIDE_WIDTH = 300        # 사이드 메뉴창 크기
+
+SCREEN_WIDTH = (ROWS * CELL_SIZE) + SIDE_WIDTH      # 화면 가로
+SCREEN_HEIGHT = (COLS * CELL_SIZE)                  # 화면 세로
+
+FPS = 30
