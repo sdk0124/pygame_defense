5fc26c62baf474e925bb0fd0b358328ee8fd6300
sdk <kimseong124@gmail.com>
2025-11-11 13:01:25 +0900

    Implement map load function

--- resource_loader.py
+++ resource_loader.py
@@ -1,10 +1,25 @@
 import pygame
+import json
 
 def load_enemy_images():
+    """적 이미지 불러오기"""
     enemy_images = {
-        "byter" : pygame.image.load("assets/images/enemy_byter.png").convert_alpha(),
-        "worm" : pygame.image.load("assets/images/enemy_worm.png").convert_alpha(),
-        "boss" : pygame.image.load("assets/images/enemy_byter.png").convert_alpha()
+        "byter" : pygame.image.load("assets/images/enemies/enemy_byter.png").convert_alpha(),
+        "worm" : pygame.image.load("assets/images/enemies/enemy_worm.png").convert_alpha(),
+        "boss" : pygame.image.load("assets/images/enemies/enemy_byter.png").convert_alpha()
     }
 
     return enemy_images
+
+def load_map_image():
+    """맵 타일 이미지 불러오기"""
+    map_image = pygame.image.load("assets/images/map/map.png")
+    
+    return map_image
+
+def load_map_data():
+    """맵 데이터 불러오기"""
+    with open('data/map_data.tmj') as file:
+        world_data = json.load(file)
+
+    return world_data
