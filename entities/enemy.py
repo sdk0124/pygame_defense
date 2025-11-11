203ef3a8854e5d70a9b6b4496ecdfe3f27195f41
sdk <kimseong124@gmail.com>
2025-11-11 13:49:18 +0900

    Implement Enemy move (include test code)

--- entities/enemy.py
+++ entities/enemy.py
@@ -2,6 +2,26 @@ import pygame
 from pygame.math import Vector2
 from data.enemy_data import ENEMY_DATA
 
+#### 임시 함수 시작 #####
+points = []
+
+def process_data(map_data):
+    # look through data to extract info
+    for layer in map_data["layers"]:
+        if layer["name"] == "waypoints":
+            for obj in layer["objects"]:
+                waypoint_data = obj["polyline"]
+                process_waypoints(waypoint_data)
+
+def process_waypoints(data):
+    # iterate through waypoints to extract x, y
+    for point in data:
+        temp_x = point.get("x")
+        temp_y = point.get("y")
+        points.append((temp_x, temp_y))
+
+#### 임시 함수 끝 #####
+
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, enemy_type, waypoints, image):
         self.type = enemy_type
@@ -23,7 +43,7 @@ class Enemy(pygame.sprite.Sprite):
     def move(self):
         """waypoint에 맞춰서 적이 이동"""
         if self.target_waypoint_idx < len(self.waypoints):
-            self.target = Vector2(self.waypoints[self.target_waypoint])
+            self.target = Vector2(self.waypoints[self.target_waypoint_idx])
             self.movement = self.target - self.position
         else:
             self.kill()
@@ -35,7 +55,9 @@ class Enemy(pygame.sprite.Sprite):
         else:
             if dist != 0:
                 self.position += self.movement.normalize() * dist
-            self.target_waypoint += 1
+            self.target_waypoint_idx += 1
+        
+        self.rect.center = self.position
 
     def draw(self, surface):
         """적 스프라이트 그리기"""
@@ -45,4 +67,41 @@ class Enemy(pygame.sprite.Sprite):
         "적 상태 갱신"
         self.move()
 
+
+### 테스트 코드 ###
+if __name__ == "__main__":
+
+    from core.settings import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
+    from resource_loader import load_enemy_images, load_map_data, load_map_image
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    map_image = load_map_image()
+    map_data = load_map_data()
+    enemy_images = load_enemy_images()
+    process_data(map_data)
+
+    print(points)
+
+    enemy_images["byter"] = pygame.transform.scale(enemy_images["byter"], (64, 64))
+
+    new_enemy = Enemy("byter", points, enemy_images["byter"])
+
+    running = True
+
+    while running:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+        screen.blit(map_image, (0, 0))
+        new_enemy.update()
+        new_enemy.draw(screen)
+        pygame.display.flip()
     
+    pygame.quit()
