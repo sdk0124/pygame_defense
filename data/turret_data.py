c1220c765a7b1d3afcdd37adedc65e62c4d29ca2
sdk <kimseong124@gmail.com>
2025-11-12 13:30:26 +0900

    Add turret data

---
+++ data/turret_data.py
@@ -0,0 +1,19 @@
+# range 단위 : pixel
+# fire_rate 단위 : ticks
+
+TURRET_DATA = {
+    "cannon" : {
+        "damage" : 25,
+        "range" : 90,
+        "fire_rate" : 1500,
+        "purchase_price" : 100,
+        "sell_price" : 70
+    },
+    "debugger" : {
+        "damage" : 60,
+        "range" : 180,
+        "fire_rate" : 3000,
+        "purchase_price" : 400,
+        "sell_price" : 200
+    }
+}
