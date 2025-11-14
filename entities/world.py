8ec219519a9ee6f0828e1f3b291c9105f8a6d070
sdk <kimseong124@gmail.com>
2025-11-14 14:35:46 +0900

    Add test code for world.py

--- entities/world.py
+++ entities/world.py
@@ -24,3 +24,41 @@ class World:
 
     def draw(self, surface):
         surface.blit(self.map_image, (0, 0))
+
+### 테스트 코드 ###
+if __name__ == "__main__":
+
+    from core.settings import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
+    from entities.enemyManager import EnemyManager
+    from resource_loader import load_map_data, load_map_image, load_enemy_images
+    from data.enemy_data import ENEMY_DATA
+    from data.wave_data import WAVE_DATA
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    world = World(load_map_data(), load_map_image())
+    world.process_data()
+
+    # print(world.get_waypoints())
+    
+    round_level = 2
+    enemy_manager = EnemyManager(world.get_waypoints(), ENEMY_DATA, load_enemy_images())
+    enemy_manager.set_wave(round_level - 1, WAVE_DATA)
+
+    running = True
+
+    while running:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        world.draw(screen)
+        enemy_manager.update(FPS)
+        enemy_manager.draw(screen)
+        pygame.display.flip()
+
+    pygame.quit()
