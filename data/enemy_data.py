7f79d4e281cbf1150ea76650cc60b91421d11775
sdk <kimseong124@gmail.com>
2025-11-14 16:21:21 +0900

    Fix enemy spawn and enemy move logic

--- data/enemy_data.py
+++ data/enemy_data.py
@@ -1,21 +1,23 @@
+"""speed 단위는 pixel. 즉 1초에 몇 pixel 움직이는가?"""
+
 ENEMY_DATA = {
     "byter" : {
         "max_hp" : 50,
-        "speed" : 2,
+        "speed" : 100,
         "money" : 50,
         "score" : 50,
         "image" : "enemy_byter.png"
     },
     "worm" : {
         "max_hp" : 30,
-        "speed" : 5,
+        "speed" : 250,
         "money" : 100,
         "score" : 100,
         "image" : "enemy_worm.png"
     },
     "boss" : {
         "max_hp" : 5000,
-        "speed" : 1,
+        "speed" : 100,
         "money" : 2000,
         "score" : 2000,
         "image" : "enemy_boss.png"
